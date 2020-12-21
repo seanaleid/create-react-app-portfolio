659b5b7512a275c896fce66290c9c2ca72ee9fdf
Sean Naleid <seannaleid@Seans-MBP.home>
2020-12-21 12:29:53 -0500

    Add About page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+// import "./App.css";
+
+// components
+import Navbar from "./components/navbar.js";
+import Home from "./components/home.js";
+import About from "./components/about.js";
+import Projects from "./components/projects.js";
+import Contact from "./components/contact.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
     </div>
   );
 }
